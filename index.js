3841eea1f09286fca14b2834c57a97873cb3e013
Gingertonic <bethmschofield@gmail.com>
2018-08-04 18:26:21 +1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much.";
+
+// XHR
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+  console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+  console.log('Booo');
+};
+
+xhr.send();
+
+// fetch()
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+// fetch with auth token
+const token = 'YOUR_TOKEN_HERE'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
